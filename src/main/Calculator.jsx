45d5af8d94e9974ea3b2311d1dee8246b6ace3d3
victorasa21 <victor.a.sa21@gmail.com>
2019-04-05 21:25:59 -0300

    feature(Calculator.jsx):Implementa a lógica das operações matemáticas

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -14,7 +14,7 @@ const InitialState = {
 
 export default class Calculator extends Component {
     
-    state = { ...InitialState}
+    state = { ...InitialState }
     constructor (props) {
         super(props)
         this.clearMemory = this.clearMemory.bind(this)
@@ -26,12 +26,48 @@ export default class Calculator extends Component {
     }
 
     setOperation(operation){
-        console.log(operation)
+        if (this.state.current === 0){
+            this.setState({ operation, current: 1, clearDisplay: true })
+        } else {
+        const equals = operation === '='
+        const currentOperation = this.state.operation
+       
+        const values = [...this.state.values]
+       
+       try{
+        values[0] = eval(`${values[0]} ${currentOperation} ${values[1]}`)
+       } catch(e){
+           values[0] = this.state.values[0]
+       }
+        values[1] = 0
+
+        this.setState({
+            displayValue: values[0],
+            operation: equals ? null : operation,
+            current: equals ? 0 : 1,
+            clearDisplay: !equals,
+            values
+        })
+    } 
     }
 
     addDigit(n) {
         if(n === '.' && this.state.displayValue.includes('.')){
              return
+        }
+
+        const clearDisplay = this.state.displayValue === '0' || this.state.clearDisplay
+        const currentValue = clearDisplay ? '' : this.state.displayValue
+        const displayValue = currentValue + n
+        this.setState({ displayValue, clearDisplay: false})
+    
+        if (n !== '.') {
+            const i = this.state.current 
+            const newValue = parseFloat(displayValue)
+            const values = [...this.state.values]
+            values[i] = newValue
+            this.setState({ values })
+            console.log(values)
         }
     }
     
@@ -43,20 +79,20 @@ export default class Calculator extends Component {
             <Button label = "AC" click={this.clearMemory} triple/>
             <Button label = "/" click={this.setOperation} operation/>
             <Button label = "7"click={this.addDigit}/>
-            <Button label = "8 " click={this.addDigit}/>
-            <Button label = "9 " click={this.addDigit}/>
-            <Button label = "* " click={this.setOperation} operation/>
-            <Button label = "4 " click={this.addDigit}/>
-            <Button label = "5 " click={this.addDigit}/>
-            <Button label = "6 " click={this.addDigit}/>
-            <Button label = "- " click={this.setOperation} operation/>
-            <Button label = "1 " click={this.addDigit}/>
-            <Button label = "2 " click={this.addDigit}/>
-            <Button label = "3 " click={this.addDigit}/>
-            <Button label = "+ " click={this.setOperation} operation/>
-            <Button label = "0 " click={this.addDigit} double/>
-            <Button label = ". " click={this.addDigit}/>
-            <Button label = "= " click={this.setOperation} operation/>
+            <Button label = "8" click={this.addDigit}/>
+            <Button label = "9" click={this.addDigit}/>
+            <Button label = "*" click={this.setOperation} operation/>
+            <Button label = "4" click={this.addDigit}/>
+            <Button label = "5" click={this.addDigit}/>
+            <Button label = "6" click={this.addDigit}/>
+            <Button label = "-" click={this.setOperation} operation/>
+            <Button label = "1" click={this.addDigit}/>
+            <Button label = "2" click={this.addDigit}/>
+            <Button label = "3" click={this.addDigit}/>
+            <Button label = "+" click={this.setOperation} operation/>
+            <Button label = "0" click={this.addDigit} double/>
+            <Button label = "." click={this.addDigit}/>
+            <Button label = "=" click={this.setOperation} operation/>
             </div>
         )
     }
